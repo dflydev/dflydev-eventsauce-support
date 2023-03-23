b53ba44110c092ac61cff5fec878f9b0880cb260
Beau Simensen <beau@dflydev.com>
2023-03-23 14:28:43 -0500

    This was faulty

--- src/Identity/IdentityInflection.php
+++ src/Identity/IdentityInflection.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Dflydev\EventSauce\Support\Identity;
 
 use EventSauce\EventSourcing\AggregateRootId;
-use EventSauce\EventSourcing\Message;
 
 final class IdentityInflection
 {
@@ -29,27 +28,5 @@ final class IdentityInflection
         assert(is_string($value), 'Expected $value to be a string if it is not null');
 
         return $identityClass::fromString($value);
-    }
-
-    /**
-     * @template T of AggregateRootId
-     *
-     * @param class-string<T> $identityClass
-     *
-     * @return T $value
-     */
-    public static function extractFromMessage(string $identityClass, Message $message): AggregateRootId
-    {
-        if ($message->aggregateRootType() !== $identityClass) {
-            throw new \RuntimeException('Expected $message to have an aggregate root type of '.$identityClass.', had '.$message->aggregateRootType().' instead.');
-        }
-
-        $value = self::toObject($identityClass, $message->aggregateRootId());
-
-        if (is_null($value)) {
-            throw new \RuntimeException('Expected $message to have an aggregate root id');
-        }
-
-        return $value;
     }
 }
