1b319c28c98350d866d1b8efa3667c4470b616cb
Beau Simensen <beau@dflydev.com>
2023-04-18 08:12:44 -0500

    Include MessageInflection

---
+++ src/Messages/MessageInflection.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Dflydev\EventSauce\Support\Messages;
+
+use Dflydev\EventSauce\Support\AggregateRoot\AggregateRootIdAware;
+use Dflydev\EventSauce\Support\Identity\IdentityInflection;
+use EventSauce\EventSourcing\AggregateRootId;
+use EventSauce\EventSourcing\ClassNameInflector;
+use EventSauce\EventSourcing\Message;
+
+final readonly class MessageInflection
+{
+    public function __construct(private ClassNameInflector $classNameInflector)
+    {
+    }
+
+    /**
+     * @template T of AggregateRootId
+     *
+     * @param class-string<T>|null $identityType
+     *
+     * @phpstan-return T|AggregateRootId $value
+     */
+    public function extractAggregateRootId(Message $message, ?string $identityType = null): AggregateRootId
+    {
+        /** @var class-string<AggregateRootIdAware>|null $aggregateRootTypeString */
+        $aggregateRootTypeString = $message->aggregateRootType();
+
+        !is_null($aggregateRootTypeString) || throw new \LogicException('Expected $message to have an aggregate root type.');
+
+        /** @phpstan-var class-string<AggregateRootIdAware> $aggregateRootType */
+        $aggregateRootType = $this->classNameInflector->typeToClassName($aggregateRootTypeString);
+
+        if (!in_array(AggregateRootIdAware::class, class_implements($aggregateRootType) ?: [])) {
+            throw new \RuntimeException("Cannot extract Aggregate Root ID from \"$aggregateRootType\" because it does not implement AggregateRootIdAware.");
+        }
+
+        $aggregateRootIdType = $aggregateRootType::aggregateRootIdClassName();
+
+        $value = IdentityInflection::toObject(
+            $identityType ?? $aggregateRootIdType,
+            $message->aggregateRootId()
+        );
+
+        if (is_null($value)) {
+            throw new \RuntimeException('Expected $message to have an aggregate root id');
+        }
+
+        return $value;
+    }
+}
