50a09de87a307376b90644ce877cfa1361df0ecd
Zaher Ghaibeh <zaher@zaher.dev>
2023-05-04 21:19:35 +0300

    remove the license header from the files

--- src/AggregateRoot/EventedAggregateRootBehaviour.php
+++ src/AggregateRoot/EventedAggregateRootBehaviour.php
@@ -2,15 +2,6 @@
 
 declare(strict_types=1);
 
-/**
- * Copyright (c) 2023 Dragonfly Development Inc
- *
- * For the full copyright and license information, please view
- * the LICENSE file that was distributed with this source code.
- *
- * @see https://github.com/dflydev/dflydev-eventsauce-support
- */
-
 namespace Dflydev\EventSauce\Support\AggregateRoot;
 
 use Dflydev\EventSauce\Support\Identity\IdentityGeneration;
